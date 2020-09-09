988ac643f70405cee8b271f74466abaa55dbd9f8
Thomas Glenn <thomasgrantglenn@mac.com>
2020-09-09 00:07:18 -0600

    add form buttons.

---
+++ help-queue/src/components/TicketControl.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+  }
+  // We recommend experimenting with adding counters, booleans, and other states that need updating to your applications to get more practice with this slightly more complex implementation of setState(). (What the hell?)
+
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList />
+      buttonText = 'Add Ticket';
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TicketControl;
